c1b89962268ec1858ed469268d6059d29004cacb
ryoichi kinoshita <rkinotoy@icloud.com>
2017-11-10 12:01:31 +0900

    Demo

---
+++ runflask.py
@@ -0,0 +1,11 @@
+﻿#coding: utf-8
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hell. Ryoichi !  Try Flask."
+
+if __name__ == "__main__":
+    app.run()
